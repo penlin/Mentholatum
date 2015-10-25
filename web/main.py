18640be2744aefdca9e0107195c498d6ac1d312b
LinKuan-yu <penlin@Penlin-2.local>
2015-10-25 16:35:14 +0800

    [M] update mainpage to baisc canlender with FullCalendar lib

--- web/main.py
+++ web/main.py
@@ -2,9 +2,11 @@
 
 import os
 import ws_api
-from bottle import route, run, template
+from bottle import route, run, template, get
 
-
+@get('/')
+def test():
+	return template('tpl/mentholatum')
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 8080))
