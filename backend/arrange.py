b174b7b95a1fd2eb31e53ff0c696220960b4ef25
Bruce Kuo <bruce3557@gmail.com>
2015-11-01 23:37:41 +0800

    Deal with input

--- backend/arrange.py
+++ backend/arrange.py
@@ -2,6 +2,38 @@
 
 import os
 import json
+import sys
+import codecs
 
-def arrange():
-  return ""
+from calendar import monthrange
+
+def arrange(nurse_path, year, month):
+  days = monthrange(year, month)
+  results = dict()
+  for x in xrange(1:days+1):
+    results[x] = {'白班': list(), '小夜': list(), '大夜': list()}
+
+  names = list()
+  levels = dict()
+  offday_dict = dict()
+
+  with codecs.open(nurse_path, 'r', encoding='utf-8') as f:
+    for line in f:
+      x = line.strip().split(',')
+      names.append(x[0])
+      levels[x[0]] = x[1]
+      offday_dict[x[0]] = x[2:]
+
+  # TODO(Bruce Kuo): Arrange
+
+  return results
+
+
+if __name__ == '__main__':
+  if len(sys.argv) < 4:
+    print "Usage: %s [nurse_path] [year] [month]" % (sys.argv[0])
+  nurse_path = sys.argv[1]
+  year = sys.argv[2]
+  month = sys.argv[3]
+  arrange(nurse_path, year, month)
+
