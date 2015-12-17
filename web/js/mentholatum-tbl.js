9cd6e8f439050c42057b0488216e036efd63a2ab
LinKuan-yu <penlin@Penlin-2.local>
2015-12-18 04:01:30 +0800

    [M] render default style of simple table with properly classified test use /2015-11 or other uri in format /[YEAR]-[MONTH]

---
+++ web/js/mentholatum-tbl.js
@@ -0,0 +1,37 @@
+
+function initMentholatumTbl(result) {
+  var headHTML = '<thead><tr><th scope="col" class="Name">名字</th>';
+  var bodyHTML = '<tbody>';
+  var day = 1;
+  $.each(result, function(name, schedule){
+    bodyHTML += '<tr class="row"><td class="name-header">' + name + '</td>';
+	schedule.forEach( function(s){
+      if (day <= 31) {
+        headHTML += '<th scope="col" class="Date">' + day + '</th>'
+        day ++;
+      }
+      bodyHTML += '<td class="normal ';
+      switch(s){
+        case '7-3':
+          bodyHTML += 'day ';
+          break;
+        case '3-11':
+          bodyHTML += 'night ';
+          break;
+        case '11-7':
+          bodyHTML += 'dawn ';
+          break;
+        case 'OFF':
+          bodyHTML += 'break ';
+          break;
+      }
+      bodyHTML += '">' + s + '</td>';
+    });
+    bodyHTML += '</tr>';
+  });
+  headHTML += '</tr></thead>';
+  bodyHTML += '</tbody>';
+  var innerHTML =  '<table class="schedule-tbl">' + headHTML + bodyHTML + '</table>';
+  console.log(innerHTML);
+  $('#Mentholatum-tbl').html(innerHTML);
+}
--- web/js/mentholatum.js
+++ web/js/mentholatum.js
@@ -31,3 +31,4 @@ function initFullCalendar(events) {
     events: events
   });
 }
+
