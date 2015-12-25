26edc4aa175b765359151a048c6ab19df504f703
LinKuan-yu <penlin@Penlin-2.local>
2015-12-25 15:13:02 +0800

    [M] support to render the schedule into xlsx sheet

--- web/download.py
+++ web/download.py
@@ -18,9 +18,17 @@ def exportSchedule(result, year, month, fmt='csv'):
   elif fmt == 'excel':
     f = StringIO.StringIO() # create a file-like object
     wbk = xlwt.Workbook()
-    wbk.add_sheet('foo')
+    wbk.add_sheet("人班表".decode("utf-8"))
     sheet = wbk.get_sheet(0)
-    sheet.write(0, 0, 'bar')
+    sheet.write(0, 0, "名字".decode("utf-8"))
+    for d in xrange(1, day+1):
+      sheet.write(0, d, str(d))
+    curRow = 1
+    for a,b in result["人班表"].iteritems():
+      sheet.write(curRow,0,a)
+      for d in xrange(1, day+1):
+        sheet.write(curRow, d, b[d-1])
+      curRow = curRow +1;
     wbk.save(f) 
     f.seek(0)
     return f.getvalue()
